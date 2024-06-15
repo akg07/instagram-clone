8294ffc2415bae4e576ff14b264df62075be619a
Ayush <ayush3032@gmail.com>
2024-06-15 23:27:34 +0530

    protected route

---
+++ middleware/requireLogin.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../keys');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+module.exports = (req, res, next) => {
+
+  const {authorization} = req.headers;
+  if(!authorization) {
+    return res.status(401).json({message: 'Please login.'});
+  }
+
+  const token = authorization.replace("Bearer ", "");
+  jwt.verify(token, JWT_SECRET, (err, payload) => {
+    if(err) {
+      return res.status(401).json({error: "Please login."})
+    }
+
+    const {_id} = payload;
+    User.findById(_id).then((userData) => {
+      req.user = userData; // modify request using middleware
+    }).catch(err => console.log(err));
+
+  });
+
+  next(); // stop this middleware and continue req-res-cycle.
+
+
+}
