9e45af65eec4eba674450f63decfaa70ca9e3a74
Ayush <ayush3032@gmail.com>
2024-06-18 01:50:39 +0530

    changes for deployment

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const { MONGO_URI } = require('./keys');
+const { MONGO_URI } = require('./config/keys');
 
 const app = express(); // invoke the express
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(MONGO_URI);
 mongoose.connection.on('connected', () => {
@@ -26,6 +26,14 @@ require('./models/post');
 app.use(require('./routes/auth'));
 app.use(require('./routes/post'));
 app.use(require('./routes/user'));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => { // run the application on PORT.
   console.log(`Server is running on ${PORT}`);
